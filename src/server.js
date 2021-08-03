a6f0b17233e97923710f1b7c39e4f95a1091c383
RansDesai <ransdesai90@gmail.com>
2021-08-03 20:03:25 +0500

    Return githash and version in response

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 var app = express();
 var port = process.env.PORT || 3000
+const git = require("git-rev-sync");
 
 // This responds a GET request for the /version page.
 app.get('/version', function (req, res) {
   console.log("Got a GET request for /version");
   res.json(
-    [
+    {
+      myapplication: [
         {
-          version: "1.0",
-          lastcommitsha: "123456",
+          version: process.env.npm_package_version,
+          lastcommitsha: git.short(),
           description: "Hello world"
         }
     ]
-  );
+  });
 })
 
 var server = app.listen(port, function () {
