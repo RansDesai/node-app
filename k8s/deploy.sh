e10738681897e178d9c1e459628abc6c79bc8891
RansDesai <ransdesai90@gmail.com>
2021-08-06 09:54:51 +0500

    Fix the deployment resources

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Deploying application resources on Kubernetes"
+kubectl create ns technical-test
 kubectl apply -f k8s/templates/Deployment.yaml -n technical-test
 kubectl apply -f k8s/templates/Service.yaml -n technical-test
 kubectl apply -f k8s/templates/hpa.yaml -n technical-test
